ee8120a3611024b8e2954f695a070b671903c10e
aryal05 <aryal.rajat05@gmail.com>
2024-11-11 13:16:51 +0545

    hero section start up

--- Ecomerce Frontend/src/components/Navbar.jsx
+++ Ecomerce Frontend/src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
    </div>
 
    {/* sideBar Menu for small screen */}
-   <div className={`aboslute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full': 'w-0'}`}>
+   <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full': 'w-0'}`}>
 
     <div className='flex flex-col text-gray-600'>
         <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3'>
