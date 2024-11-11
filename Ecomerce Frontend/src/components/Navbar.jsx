296bb7c43560a7fb9c602a97ab5c6c5e74f0122c
aryal05 <aryal.rajat05@gmail.com>
2024-11-11 11:43:58 +0545

    navbar section icons

--- Ecomerce Frontend/src/components/Navbar.jsx
+++ Ecomerce Frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {assets} from "../assets/assets"
-import {NavLink} from "react-router-dom"
+import {Link, NavLink} from "react-router-dom"
 const Navbar = () => {
   return (
    <>
@@ -26,6 +26,30 @@ const Navbar = () => {
         <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
     </NavLink>
    </ul>
+
+   <div className='flex items-center gap-6'>
+    <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+
+    <div className='group relative'>
+        <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
+
+        <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-0'>
+          <div className='flex flex-col gap-2 w-36 py-3 bg-slate-100 text-gray-500 rounded'>
+            <p className='cursor-pointer hover:text-black'>My Profile</p>
+            <p className='cursor-pointer hover:text-black'>Orders</p>
+            <p className='cursor-pointer hover:text-black'>Logout</p>
+
+          </div>
+        </div>
+
+    </div>
+    <Link to = "/cart" className='relative'>
+    <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text[8px]'>9</p>
+    </Link>
+    <img src={assets.menu_icon} alt="" />
+
+   </div>
    </div>
    </>
   )
