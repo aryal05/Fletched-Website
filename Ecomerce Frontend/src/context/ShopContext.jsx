0ba4ddb308e43cbb8c60a521ddc99ea0ac16072c
aryal05 <aryal.rajat05@gmail.com>
2024-11-11 18:42:59 +0545

    latest collection

--- Ecomerce Frontend/src/context/ShopContext.jsx
+++ Ecomerce Frontend/src/context/ShopContext.jsx
@@ -1,27 +1,21 @@
-import{createContext} from "react"
-import {products} from "../assets/assets"
+import { createContext } from "react";
+import { products } from "../assets/assets";
 
+export const ShopContext = createContext();
 
+const ShopContextProvider = (props) => {
+    const currency = '$';
+    const delivery_fee = '10';
 
-export const ShopContext =createContext();
-
-const ShopContextProvider =()=>{
-    const currecy = '$'
-    const delivery_fee = '10'
-    
     const value = {
-        products, currecy, delivery_fee
+        products, currency, delivery_fee
+    };
 
-    }
-
-    return(
-        <ShopContextProvider>
-
+    return (
+        <ShopContext.Provider value={value}>
             {props.children}
-
-
-        </ShopContextProvider>
-    )
-}
+        </ShopContext.Provider>
+    );
+};
 
 export default ShopContextProvider;
--- Ecomerce Frontend/src/pages/Home.jsx
+++ Ecomerce Frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import LatestCoollectioon from '../components/LatestCoollectioon'
+import LatestCollectioon from '../components/LatestCollection'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
-      <LatestCoollectioon/>
+      <LatestCollectioon/>
     </div>
   )
 }
