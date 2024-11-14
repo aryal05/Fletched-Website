b405b347f1635ad256398b9eddd179fb875ee1aa
aryal05 <aryal.rajat05@gmail.com>
2024-11-14 19:40:36 +0545

    Best Seller Section Completion

--- Ecomerce Frontend/src/components/BestSeller.jsx
+++ Ecomerce Frontend/src/components/BestSeller.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 import {ShopContext} from '../context/ShopContext'
 import { useEffect } from 'react'
 import Title from './Title'
+import ProductItem from './ProductItem';
 const BestSeller = () => {
     const {products} = useContext(ShopContext);
 
@@ -19,6 +20,14 @@ const BestSeller = () => {
             <p className='w-3/4 m-auto text-xs sn:text-sm md:text-base text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odit
                 eaque quo vero dicta? Similique.</p>
 
+        
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+        {
+        bestSeller.map((item, index)=>(
+          <ProductItem key={index} id ={item._id} image={item.image} name={item.name} price={item.price} />
+        ))
+       }
+</div>
         </div>
       
     </div>
