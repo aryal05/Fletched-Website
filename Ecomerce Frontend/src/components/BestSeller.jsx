1b5d07abb8febb43d1a35a9565f30af625bd5539
aryal05 <aryal.rajat05@gmail.com>
2024-11-14 20:00:36 +0545

    bes seller section code setup

--- Ecomerce Frontend/src/components/BestSeller.jsx
+++ Ecomerce Frontend/src/components/BestSeller.jsx
@@ -19,15 +19,15 @@ const BestSeller = () => {
            <Title text1={"BEST"} text2={"SELLERS"}/>
             <p className='w-3/4 m-auto text-xs sn:text-sm md:text-base text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odit
                 eaque quo vero dicta? Similique.</p>
-
+                </div>
         
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
-        {
+       {
         bestSeller.map((item, index)=>(
-          <ProductItem key={index} id ={item._id} image={item.image} name={item.name} price={item.price} />
+            <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
         ))
        }
-</div>
+
         </div>
       
     </div>
