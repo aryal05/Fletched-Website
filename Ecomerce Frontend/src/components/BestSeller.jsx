c47270a7d3373400e11850971b3c84da7bb6d580
aryal05 <aryal.rajat05@gmail.com>
2024-11-14 19:35:03 +0545

    Best Seller

--- Ecomerce Frontend/src/components/BestSeller.jsx
+++ Ecomerce Frontend/src/components/BestSeller.jsx
@@ -1,17 +1,25 @@
 import React, { useContext, useState } from 'react'
 import {ShopContext} from '../context/ShopContext'
 import { useEffect } from 'react'
+import Title from './Title'
 const BestSeller = () => {
-    const {products} = useContext(ShopContext)
-    const [bestSeller, setBestSeller] = useState([])
+    const {products} = useContext(ShopContext);
+
+    const [bestSeller, setBestSeller] = useState([]);
 
     useEffect(()=>{
-        const bestProduct = products.filter((item) => item.bestSeller)
+        const bestProduct = products.filter((item) => item.bestseller)
         setBestSeller(bestProduct.slice(0,5))
 
     },[])
   return (
-    <div>
+    <div className='my-10'>
+        <div className='text-center text-3xl py-8'>
+           <Title text1={"BEST"} text2={"SELLERS"}/>
+            <p className='w-3/4 m-auto text-xs sn:text-sm md:text-base text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odit
+                eaque quo vero dicta? Similique.</p>
+
+        </div>
       
     </div>
   )
--- Ecomerce Frontend/src/pages/Home.jsx
+++ Ecomerce Frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollectioon from '../components/LatestCollection'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestCollectioon/>
+      <BestSeller/>
     </div>
   )
 }
