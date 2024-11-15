42210e74e62774dc0cc1273488be8b7bb5414568
aryal05 <aryal.rajat05@gmail.com>
2024-11-15 20:17:52 +0545

    footer section completed

--- Ecomerce Frontend/src/components/Footer.jsx
+++ Ecomerce Frontend/src/components/Footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+            <div>
+                <img src={assets.logo} className='mb-5 w-32' alt="" />
+                <p className='w-full md:w-2/3 text-gray-600'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate dignissimos molestiae consequuntur consequatur reiciendis nihil.
+                </p>
+            </div>
+            <div>
+                <p className='text-xl font-medium mb-5'>Company</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>9734525634212</li>
+                    <li>bauno@bauni</li>
+                 
+                </ul>
+            </div>
+
+        
+            </div>
+
+        </div>
       
-    </div>
+   
   )
 }
 
