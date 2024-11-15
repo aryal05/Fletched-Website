cfb853ef1ccdb50ccba2018ad6ed73b2152325b0
aryal05 <aryal.rajat05@gmail.com>
2024-11-15 20:04:01 +0545

    onsubmit functionality added

--- Ecomerce Frontend/src/components/NewsLetterBox.jsx
+++ Ecomerce Frontend/src/components/NewsLetterBox.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const NewsLetterBox = () => {
+    const onSubmitHandler=(event)=>{
+        event.preventDefault();
+
+    }
   return (
     <div className='text-center'>
          <p className='text-2xl font-medium text-gray-800'>Subscribe now & get exclusive updates!</p>
          <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, pariatur?</p>
-         <form className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 borrder pl-3'>
+         <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 borrder pl-3'>
             <input className='w-full sm:flex-1 outline-none' type='email' placeholder='Enter Your Email'required />
             <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
          </form>
