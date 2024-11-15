c31a36e05a1e99f829344b15dcf0b95e0b4722dc
aryal05 <aryal.rajat05@gmail.com>
2024-11-15 12:06:13 +0545

    news letter

---
+++ Ecomerce Frontend/src/components/NewsLetterBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NewsLetterBox = () => {
+  return (
+    <div className='text-center'>
+         <p className='text-2xl font-medium text-gray-800'>Subscribe now & get exclusive updates!</p>
+
+    </div>
+  )
+}
+
+export default NewsLetterBox 
+
