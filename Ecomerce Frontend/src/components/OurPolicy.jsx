13c504dee05554941d45397372212d178a0e3d4f
aryal05 <aryal.rajat05@gmail.com>
2024-11-15 11:53:56 +0545

    ourpoliy sevction setup

---
+++ Ecomerce Frontend/src/components/OurPolicy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OurPolicy = () => {
+  return (
+    <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700'>
+      
+    </div>
+  )
+}
+
+export default OurPolicy
