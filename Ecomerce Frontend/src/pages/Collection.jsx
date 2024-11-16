74ebf155822337d799fcb8db29f6c59848979618
aryal05 <aryal.rajat05@gmail.com>
2024-11-16 15:10:58 +0545

    collection sexction updation

--- Ecomerce Frontend/src/pages/Collection.jsx
+++ Ecomerce Frontend/src/pages/Collection.jsx
@@ -2,10 +2,16 @@ import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets'
 import Title from '../components/Title'
+import { useEffect } from 'react'
+import ProductItem from '../components/ProductItem'
 
 const Collection = () => {
   const {products} = useContext(ShopContext)
   const [showFilter, setShowFilter] = useState(false)
+  const[filterProducts, setFilterProducts] = useState([])
+  useEffect(()=>{
+    setFilterProducts(products)
+  },[])
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       
@@ -61,6 +67,15 @@ const Collection = () => {
           <option value="high-low">Sort by: High to Low</option>
         </select>
 
+        </div>
+
+        {/* Map Product */}
+        <div className='grid grid-cols-2 mg:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+            {
+              filterProducts.map((item,index)=>(
+                <ProductItem key={index} name={item.name} image={item.image} price={item.price} id={item._id} />
+              ))
+            }
         </div>
 
       </div>
