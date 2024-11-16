8f9a1f4e8e2c55604413abc080c625d8971c4265
aryal05 <aryal.rajat05@gmail.com>
2024-11-16 13:29:48 +0545

    subcategory

--- Ecomerce Frontend/src/pages/Collection.jsx
+++ Ecomerce Frontend/src/pages/Collection.jsx
@@ -26,7 +26,22 @@ const Collection = () => {
           </p>
         </div>
 </div>
-             
+             {/* SubCategory */}
+             <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+        <p className='mb-3 text-sm font-medium'>TYPES</p>
+        <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+          <p className='flex gap-2'>
+            <input type="checkbox" className='w-3' value={'Topwear'} />Topwear
+          </p>
+          <p className='flex gap-2'>
+            <input type="checkbox" className='w-3' value={'Bottomwear'} />Bottomwear
+          </p>
+          <p className='flex gap-2'>
+            <input type="checkbox" className='w-3' value={'Winterwear'} />Winterwear
+          </p>
+        </div>
+</div>
+
 
       </div>
 
