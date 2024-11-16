83d439fe1daf95811fc1e32340a0fbbbdac97d53
aryal05 <aryal.rajat05@gmail.com>
2024-11-16 12:30:26 +0545

    filter section

--- Ecomerce Frontend/src/pages/Collection.jsx
+++ Ecomerce Frontend/src/pages/Collection.jsx
@@ -6,6 +6,17 @@ const Collection = () => {
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       
+     {/* Filter Options */}
+     <div className='min-w-60'>
+      <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+
+      {/* Category Filter */}
+      <div className={`border border-gray-300 pl-5 py-3 mt-6`}>
+
+      </div>
+
+     </div>
+
     </div>
   )
 }
