e26a0f7f2db4b2628f3fffb977e8a0468c3cd653
aryal05 <aryal.rajat05@gmail.com>
2024-11-17 19:47:00 +0545

    Start up of about section

--- Ecomerce Frontend/src/pages/About.jsx
+++ Ecomerce Frontend/src/pages/About.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const About = () => {
   return (
     <div>
+      <div>
+        Starting with a blank slate, you can create a new React app using the following command in your terminal:
+      </div>
       
     </div>
   )
