a3eaaba4548baa11adcb9ddd1c70de7bad562282
aryal05 <aryal.rajat05@gmail.com>
2024-11-18 19:43:41 +0545

    search and cross icon

--- Ecomerce Frontend/src/components/SearchBar.jsx
+++ Ecomerce Frontend/src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets'
 
 const SearchBar = () => {
     const {search, setSearch, showSearch, setShowSearch} = useContext(ShopContext)
@@ -8,8 +9,10 @@ const SearchBar = () => {
 
     <div className='border-t border-b bg-gray-50 text-center'>
         <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
-          <input type="text" placeholder='Search' />
+          <input value={search} onChange={(e)=>setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text-sm' type="text" placeholder='Search' />
+          <img className='w-4' src={assets.search_icon} alt="" />
         </div>
+        <img className='' src={assets.cross_icon} alt="" />
       
     </div>
   ):null
