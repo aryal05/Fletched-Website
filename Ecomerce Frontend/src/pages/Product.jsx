89a73534d02f6467cdffc124ac2e9bb78fae5dcf
aryal05 <aryal.rajat05@gmail.com>
2024-11-19 21:02:14 +0545

    useState and function is made

--- Ecomerce Frontend/src/pages/Product.jsx
+++ Ecomerce Frontend/src/pages/Product.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
-  const {prroductId} =useParams
-  return (
+  const {productId} =useParams();
+  const {products} = useContext(ShopContext)
+  const [productData, setProductData] = useState(false)
+  const [image, setImage] = useState('')
+
+  const fetchPoductData = async()=>{
+
+    products.map((item)=>{
+      if(item.id === productId){
+        setProductData(item)
+        setImage(item.image[0])
+
+        return null;
+      }
+    })
+
+  }
+
+  useEffect(()=>{
+    fetchPoductData();
+
+  },[productId, products])
+
+
+
+  return productData? (
+
     <div>
 
       
     </div>
-  )
+  ):<div className='opacity-0'></div>
 }
 
 export default Product
