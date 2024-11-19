edc247a874d924e59fef240f35d939678e75ff87
aryal05 <aryal.rajat05@gmail.com>
2024-11-19 19:48:41 +0545

    start up of poduct page for each id

--- Ecomerce Frontend/src/pages/Product.jsx
+++ Ecomerce Frontend/src/pages/Product.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Product = () => {
+  const {prroductId} =useParams
   return (
     <div>
+
       
     </div>
   )
