c7f593bc327982348711c1b6d91a44f64569d509
aryal05 <aryal.rajat05@gmail.com>
2024-11-19 21:15:50 +0545

    product is diisplayed accordind to the id

--- Ecomerce Frontend/src/pages/Product.jsx
+++ Ecomerce Frontend/src/pages/Product.jsx
@@ -35,7 +35,16 @@ const Product = () => {
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'> 
 
       {/* Porduct Image */}
-      <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'></div>
+      <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+        <div className='flex sm:flex-col overflow-x-auto sm:overflow-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+          {
+            productData.image.map((item, index)=>(
+              <img src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0'  alt="" />
+            ))
+          }
+
+        </div>
+      </div>
 
       </div>
 
