d0b10faf0499e3fb69a272f81e17a3daf84f9033
aryal05 <aryal.rajat05@gmail.com>
2024-11-22 19:59:55 +0545

    related products tiltle added

--- Ecomerce Frontend/src/components/RelatedProduct.jsx
+++ Ecomerce Frontend/src/components/RelatedProduct.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
 
 const RelatedProduct = ({category,subCategory}) => {
     const {products} = useContext(ShopContext);
@@ -10,11 +11,16 @@ const RelatedProduct = ({category,subCategory}) => {
             let productCopy = products.slice();
             productCopy = productCopy.filter((item) => category === item.category)
             productCopy = productCopy.filter((item) => subCategory === item.subCategory)
+
+            setRelated(productCopy.slice(0,5))
         }
-    })
+    },[products])
 
   return (
-    <div>
+    <div className='my-24'>
+        <div className='text-center text-3xl py-2'>
+            <Title text1={'RELATED'} text2={'PRODUCTS'}/>
+        </div>
        
     </div>
   )
