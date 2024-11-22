d2ded977b932b8200eecbc125f8e9e1e63a87d01
aryal05 <aryal.rajat05@gmail.com>
2024-11-22 18:11:18 +0545

    product section further updation start up

--- Ecomerce Frontend/src/pages/Product.jsx
+++ Ecomerce Frontend/src/pages/Product.jsx
@@ -73,6 +73,7 @@ const Product = () => {
           </div>
         </div>
         <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+        <hr className='mt-8 sm:w-4/5' />
       </div>
 
       </div>
