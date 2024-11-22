195db5fbe209ff31e55a7d7bdcf6d66d95462e1c
aryal05 <aryal.rajat05@gmail.com>
2024-11-22 19:28:48 +0545

    related products products and usestate implementation

--- Ecomerce Frontend/src/components/RelatedProduct.jsx
+++ Ecomerce Frontend/src/components/RelatedProduct.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
-const RelatedProduct = () => {
+const RelatedProduct = ({category,subCategory}) => {
+    const {products} = useContext(ShopContext);
+    const [related, setRelated] = useState('')
+  
+    useEffect(()=>{
+        if(products.length> 0){
+            let productCopy = products.slice();
+            productCopy = productCopy.filter((item) => category === item.category)
+            productCopy = productCopy.filter((item) => subCategory === item.subCategory)
+        }
+    })
+
   return (
     <div>
-      
+       
     </div>
   )
 }
