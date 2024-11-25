782dc600b3e9b8b97cf606027e38d3999146c5fe
aryal05 <aryal.rajat05@gmail.com>
2024-11-25 20:27:06 +0545

    cart section some of the logic is wrrritten

--- Ecomerce Frontend/src/context/ShopContext.jsx
+++ Ecomerce Frontend/src/context/ShopContext.jsx
@@ -8,8 +8,29 @@ const ShopContextProvider = (props) => {
     const delivery_fee = '10';
     const [search, setSearch] = useState('')
     const [showSearch, setShowSearch] = useState(false)
-    const [cart, setCart] = useState({})
-    
+    const [cartItems, setCartItems] = useState({})
+
+    const addToCart = async(itemId,size) =>{
+        let cartData = structuredClone(cartItems); 
+
+        if(cartData[itemId]){
+            if(condition){
+                if(cartData[itemId][size]){
+                    cartData[itemId][size] += 1;
+                }else{
+                    cartData[itemId][size] = 1;
+                    
+                }
+            }
+            else{
+                cartData[itemId] = {}
+                cartData[itemId][size] =1;
+            }
+            setCartItems(cartData)
+        }
+    }
+
+
     const value = {
         products, currency, delivery_fee,search,setSearch,showSearch,setShowSearch
     };
